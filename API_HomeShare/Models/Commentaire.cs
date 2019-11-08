d8d40e4d720e96a18df2feaaa2d6bda5d917819d
ToSu <surin.thomas@hotmail.com>
2019-11-08 14:06:11 +0100

    creations model bien , commentaire et pays

---
+++ API_HomeShare/Models/Commentaire.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_HomeShare.Models
+{
+    public class Commentaire
+    {
+        #region properties
+
+        private long id;
+        private string message;
+        private int note;
+        private bool valide;
+
+
+
+        #endregion
+
+        #region getter setter
+
+        public long Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+
+            set
+            {
+                message = value;
+            }
+        }
+
+        public int Note
+        {
+            get
+            {
+                return note;
+            }
+
+            set
+            {
+                note = value;
+            }
+        }
+
+        public bool Valide
+        {
+            get
+            {
+                return valide;
+            }
+
+            set
+            {
+                valide = value;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ API_HomeShare/Models/Pays.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_HomeShare.Models
+{
+    public class Pays
+    {
+        private long _id;
+        private string nom;
+
+        public long Id
+        {
+            get
+            {
+                return _id;
+            }
+
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+    }
+}
