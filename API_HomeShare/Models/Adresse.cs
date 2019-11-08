43254e43629a763a4b22a7fe253eb68315adf620
forma308 <forma308@FORMA308>
2019-11-08 14:09:08 +0100

    creation adresse,option,assurance

---
+++ API_HomeShare/Models/Adresse.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_HomeShare.Models
+{
+    public class Adresse
+    {
+        private int _id_adresse;
+        private string _ville;
+        private int _cp;
+        private string _rue;
+        private int _num;
+        private string _boite;
+        private int _id_pays;
+
+        public int Id_adresse
+        {
+            get
+            {
+                return _id_adresse;
+            }
+
+            set
+            {
+                _id_adresse = value;
+            }
+        }
+
+        public string Ville
+        {
+            get
+            {
+                return _ville;
+            }
+
+            set
+            {
+                _ville = value;
+            }
+        }
+
+        public int Cp
+        {
+            get
+            {
+                return _cp;
+            }
+
+            set
+            {
+                _cp = value;
+            }
+        }
+
+        public string Rue
+        {
+            get
+            {
+                return _rue;
+            }
+
+            set
+            {
+                _rue = value;
+            }
+        }
+
+        public int Num
+        {
+            get
+            {
+                return _num;
+            }
+
+            set
+            {
+                _num = value;
+            }
+        }
+
+        public string Boite
+        {
+            get
+            {
+                return _boite;
+            }
+
+            set
+            {
+                _boite = value;
+            }
+        }
+
+        public int Id_pays
+        {
+            get
+            {
+                return _id_pays;
+            }
+
+            set
+            {
+                _id_pays = value;
+            }
+        }
+    }
+}
---
+++ API_HomeShare/Models/Options.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_HomeShare.Models
+{
+    public class Options
+    {
+        private int _id_option;
+        private string _nom;
+
+        public int Id_option
+        {
+            get
+            {
+                return _id_option;
+            }
+
+            set
+            {
+                _id_option = value;
+            }
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return _nom;
+            }
+
+            set
+            {
+                _nom = value;
+            }
+        }
+    }
+}
