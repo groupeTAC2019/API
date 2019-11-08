d2b4a0274e4ae7ec682a046d75f7cb6e7341041c
corentingoo <corentingoo@gmail.com>
2019-11-08 13:57:15 +0100

    ajout modele Piece

---
+++ API_HomeShare/Models/Piece.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Piece
+{
+    private int _id_piece;
+    private string _nom;
+
+    public int Id_piece
+    {
+        get
+        {
+            return _id_piece;
+        }
+
+        set
+        {
+            _id_piece = value;
+        }
+    }
+
+    public string Nom
+    {
+        get
+        {
+            return _nom;
+        }
+
+        set
+        {
+            _nom = value;
+        }
+    }
+}
