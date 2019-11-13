1be6ea07efac93cbbaa978d125754b64a206a79e
forma308 <forma308@FORMA308>
2019-11-13 12:21:39 +0100

    initialisation commentairecontoleurs

--- API_HomeShare/Controllers/CommentaireController.cs
+++ API_HomeShare/Controllers/CommentaireController.cs
@@ -23,7 +23,7 @@ namespace API_HomeShare.Controllers
         public List<Commentaire> Get()
         {
             Command cmd = new Command("Select * from Commentaire");
-            Connection con = new Connection(GetConnectionStrings("varcon").ProviderName, GetConnectionStrings("varcon").ConnectionString);
+            Connection con = new Connection(GetConnectionStrings("DBConnection").ProviderName, GetConnectionStrings("DBConnection").ConnectionString);
 
             DataTable dt = con.GetDataTable(cmd);
             List<Commentaire> comt = new List<Commentaire>();
@@ -32,7 +32,7 @@ namespace API_HomeShare.Controllers
                 Commentaire co = new Commentaire()
                 {
                     Id = (int)item["id_commentaire"],
-                    Message = (string)item["mesage"],
+                    Message = (string)item["message"],
                     Note = (int)item["note"],
                     Valide = (bool)item["valide"],
                     Id_membre = (int)item["id_membre"],
@@ -50,7 +50,7 @@ namespace API_HomeShare.Controllers
         {
             Command cmd = new Command("Select * from Commentaire where id_commentaire = @id");
             cmd.AddParameter("id", id);
-            Connection con = new Connection(GetConnectionStrings("varcon").ProviderName, GetConnectionStrings("varcon").ConnectionString);
+            Connection con = new Connection(GetConnectionStrings("DBConnection").ProviderName, GetConnectionStrings("DBConnection").ConnectionString);
 
 
             DataTable dt = con.GetDataTable(cmd);
@@ -68,13 +68,13 @@ namespace API_HomeShare.Controllers
         }
 
         // POST: api/Commentaire
-        [Route("api/Adresse")]
+        [Route("api/commentaire")]
         public Commentaire Post(Commentaire comentair)
         {
             Command cmd = new Command(@"INSERT INTO [dbo].[commentaire]
             ([message], [note], [valide], [id_membre], [id_bien] )
 
-             output inserted.id
+             output inserted.id_commentaire
 
             VALUES ( @message, @note, @valide, @id_membre, @id_bien )  ");
 
@@ -84,15 +84,24 @@ namespace API_HomeShare.Controllers
             cmd.AddParameter("id_membre", comentair.Id_membre);
             cmd.AddParameter("id_bien", comentair.Id_bien);
 
-            Connection con = new Connection(GetConnectionStrings("varcon").ProviderName, GetConnectionStrings("varcon").ConnectionString);
+            Connection con = new Connection(GetConnectionStrings("DBConnection").ProviderName, GetConnectionStrings("DBConnection").ConnectionString);
             int sid = (int)con.ExecuteScalar(cmd);
             comentair.Id = sid;
             return comentair;
         }
 
         // PUT: api/Commentaire/5
-        public void Put(int id, [FromBody]string value)
+        [Route("api/commentaire/{id:int}")]
+        [HttpPut]
+        public void Put( int id, Commentaire comt)
         {
+            Command cmd = new Command(@"update [dbo].[commentaire] set [message]=@message, [note]=@note where id_commentaire = @id");
+            cmd.AddParameter("id", id);
+            cmd.AddParameter("message", comt.Message);
+            cmd.AddParameter("note", comt.Note);
+
+            Connection con = new Connection(GetConnectionStrings("DBConnection").ProviderName, GetConnectionStrings("DBConnection").ConnectionString);
+            /*int sid = (int)*/con.ExecuteNonQuery(cmd);
         }
 
         //// DELETE: api/Commentaire/5
